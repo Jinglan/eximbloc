e08c9a12ab3eb4d952ba7498f4c8d53a5c36f7ce
jinglan wang <jwang5@wellesley.edu>
2016-02-03 14:47:19 -0500

    more var names changed

--- contracts/Payout.sol
+++ contracts/Payout.sol
@@ -1,24 +1,24 @@
 contract Payout {
-     address Victor;
-     address Jim;
-     address Kieren;
+     address importer;
+     address exporter;
+     address eximchain;
 
      mapping (address => uint) ownershipDistribution; 
 
      function Setup() {
-       Victor = 0xaabb;
-       Jim    = 0xccdd;
-       Kieren = 0xeeff;
+       importer = 0xaabb;
+       exporter    = 0xccdd;
+       eximchain = 0xeeff;
 
-       ownershipDistribution[Victor] = 35;
-       ownershipDistribution[Jim]  = 35;
-       ownershipDistribution[Kieren] = 30;
+       ownershipDistribution[importer] = 35;
+       ownershipDistribution[exporter]  = 35;
+       ownershipDistribution[eximchain] = 30;
      }
 
      function Dividend() {
        uint bal= this.balance;
-       Victor.send(bal * ownershipDistribution[Victor] / 100); 
-       Jim.send(bal * ownershipDistribution[Jim] / 100);
-       Kieren.send(bal * ownershipDistribution[Kieren] / 100);
+       importer.send(bal * ownershipDistribution[importer] / 100); 
+       exporter.send(bal * ownershipDistribution[exporter] / 100);
+       eximchain.send(bal * ownershipDistribution[eximchain] / 100);
      }
 }
--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -1,48 +1,48 @@
 contract SimpleMultiSig {
-     address alice1;
-     address alice2;
-     address bob;
+     address exporter;
+     address importer;
+     address eximchain;
      uint numSigned = 0;
      bytes32 error;
      bool registeredYet;
      mapping (address => bool) signedYet;
 
      function SimpleMultiSig() {
-       bob = msg.sender;
+       eximchain = msg.sender;
        registeredYet = false;
      }
 
-     function register(address registerAlice1, address registerAlice2) {
-       if (msg.sender == bob && registeredYet == false) {
-           alice1 = registerAlice1;
-           alice2 = registerAlice2;
+     function register(address registerexporter, address registerimporter) {
+       if (msg.sender == eximchain && registeredYet == false) {
+           exporter = registerexporter;
+           importer = registerimporter;
            registeredYet = true;
-       } else if (msg.sender == bob) {
+       } else if (msg.sender == eximchain) {
            error = "registered already";
        } else {
-         error = "you are not bob!";
+         error = "you are not eximchain!";
        }
    }
 
    function withdraw(address to) {
-     if ((msg.sender == alice1 || msg.sender == alice2) && numSigned >= 2) {
+     if ((msg.sender == exporter || msg.sender == importer) && numSigned >= 2) {
         to.send(this.balance);
         numSigned = 0;
-       signedYet[alice1] = signedYet[alice2] = signedYet[bob] = false;
+       signedYet[exporter] = signedYet[importer] = signedYet[eximchain] = false;
      } else {
         error = "cannot withdraw yet!";
      }
    }
 
    function addSignature() {
-     if (msg.sender == alice1 && signedYet[alice1]==false) {
-       signedYet[alice1] = true;
+     if (msg.sender == exporter && signedYet[exporter]==false) {
+       signedYet[exporter] = true;
        numSigned++;
-     } else if (msg.sender == alice2 && signedYet[alice2]==false) {
-       signedYet[alice2] = true;
+     } else if (msg.sender == importer && signedYet[importer]==false) {
+       signedYet[importer] = true;
        numSigned++;
-     } else if (msg.sender == bob && signedYet[bob]==false) {
-       signedYet[bob] = true;
+     } else if (msg.sender == eximchain && signedYet[eximchain]==false) {
+       signedYet[eximchain] = true;
        numSigned++;
      } else {
        error = 'unknown address';
